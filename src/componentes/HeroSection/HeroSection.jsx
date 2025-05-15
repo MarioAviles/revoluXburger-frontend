0455f2690a107edf3822b08d21dea3e63bff8675
MarioAviles <4150123@alu.murciaeduca.es>
2025-05-15 18:31:19 +0200

    He cambio la hero section, te he dejado comentado todo lo que tenias antes por si acaso

--- src/componentes/HeroSection/HeroSection.jsx
+++ src/componentes/HeroSection/HeroSection.jsx
@@ -1,8 +1,14 @@
 import './HeroSection.css';
 import { Link } from 'react-router-dom';
+import burger from '../../assets/video/videoBurgerGirando.gif';
 const HeroSection = () => {
   return (
     <div className="hero-section">
+      <img
+        src= {burger}
+        alt="Hamburguesa girando"
+        className="hero-burger"
+      />
       <div className="overlay">
         <h1 className="hero-title">RevoluXBurger</h1>
         <p className="hero-subtitle">"Más que burger... ¡es <strong className='amarillo'>Revolux</strong>!"</p>
